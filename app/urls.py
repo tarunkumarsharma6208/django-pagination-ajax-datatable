ffcee797fe780cd402615df457737e0e8906decd
Tarun <tarun.kumar@tech58.co.in>
2023-06-23 12:00:07 +0530

    code clean

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from app.views import *
 
 urlpatterns = [
-    # path('latestnews/', display_latestnews, name="display_pagination"),
-    # path('mymodel/', MyModelListView.as_view(), name='mymodel-list'),
-    
     path('mymodel_json/', mymodel_json, name='mymodel_json'),
     path('mymodel/', mymodel_list, name='mymodel-list'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,21 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import MyModel
-from django_datatables_view.base_datatable_view import BaseDatatableView
 from django.views.decorators.http import require_POST
 
-
-# class MyModelListView(BaseDatatableView):
-#     model = MyModel
-#     columns = ['id', 'name', 'price']
-
-#     def render_column(self, row, column):
-#         # Customize how each column is rendered if needed
-#         if column == 'name':
-#             return row.name
-#         elif column == 'price':
-#             return row.price
-#         return super().render_column(row, column)
     
 @require_POST
 def mymodel_json(request):
@@ -62,53 +49,6 @@ def mymodel_json(request):
 def mymodel_list(request):
     return render(request, 'index.html.j2')
 
-# def my_model_list(request):
-#     queryset = MyModel.objects.all()
-#     paginator = Paginator(queryset, 10)  # Show 10 items per page
 
-#     page_number = request.POST.get('start')  # DataTables sends the starting index of the page
-#     page_obj = paginator.get_page(page_number // 10 + 1)  # Calculate the page number based on the starting index
-
-#     data = []
-#     for obj in page_obj:
-#         data.append({
-#             'id': obj.id,
-#             'name': obj.name,
-#             'price': obj.price
-#         })
-
-#     return JsonResponse({'draw': int(request.POST.get('draw', 0)), 'recordsTotal': queryset.count(), 'recordsFiltered': queryset.count(), 'data': data})
-
-
-# def display_latestnews(request):
-#     newsdata = MyModel.objects.all()
-#     # articles per page
-#     per_page = 4
-#     # Paginator in a view function to paginate a queryset
-#     # show 4 news per page
-#     obj_paginator = Paginator(newsdata, per_page)
-
-#     context = {
-#         'obj_paginator': obj_paginator,
-#     }
-
-#     if request.method == 'POST':
-#         # getting page number
-#         page_no = request.POST.get('page_no', None)
-#         try:
-#             requested_page = obj_paginator.page(page_no)
-#         except (PageNotAnInteger, EmptyPage):
-#             requested_page = obj_paginator.page(1)
-
-#         results = list(requested_page.object_list.values('id', 'name', 'price'))
-#         return JsonResponse({"results": results})
-
-#     first_page = obj_paginator.page(1)
-#     page_range = obj_paginator.page_range
-
-#     context['first_page'] = first_page
-#     context['page_range'] = page_range
-
-#     return render(request, 'index.html.j2', context)
 
 
